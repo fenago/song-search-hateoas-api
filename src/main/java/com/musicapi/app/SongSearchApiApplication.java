8e2596fb07e47ed90195f8a4e97276100261c8c8
Dr. Ernesto Lee <socrates73@gmail.com>
2025-07-02 07:56:52 -0400

    Add files via upload

---
+++ src/main/java/com/musicapi/app/SongSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.musicapi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicapi/app/SongSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicapi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
